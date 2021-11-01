5efcdb1dbd7211054a806b675e30cf230aa9b3b8
Evgeni <my@example.com>
2021-11-01 11:23:08 +0300

    new rep

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
-  <div id="app">
+  <div id="app" class="back">
+    <div id="about">
+      <h1>Hi 👋</h1>
+      <p>My name is Sergei, I am an DevOps Engineer and you are on my webpage.</p>
+    </div>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    
+    <!-- borders -->
+    <div class="line top"></div>
+    <div class="line bottom"></div>
+    <div class="line left"></div>
+    <div class="line right"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
-  }
+    // HelloWorld
+  },
+  data() {
+    return {
+      firstName: 'Serov',
+      name: 'Sergei',
+    }
+  },
 }
 </script>
 
@@ -24,5 +40,44 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#app #about{
+  color: blueviolet;
+}
+#app .line{
+  background: #26262d;
+  content: '';
+  position: fixed;
+  background: #26262d;
+  z-index: 105;
+}
+#app .line.top{
+    /* left: 0; */
+    top: 0;
+    width: 100%;
+    height: 30px;
+}
+#app .line.bottom{
+    left: 0;
+    top: auto;
+    bottom: 0;
+    width: 100%;
+    height: 30px;
+}
+#app .line.left{
+    left: 0;
+    top: 0;
+    width: 30px;
+    height: 200%;
+}
+#app .line.right{
+    left: auto;
+    right: 0;
+    top: 0;
+    width: 30px;
+    height: 200%;
+}
+.back{
+  background-color: #31313a;
 }
 </style>
