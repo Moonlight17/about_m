8f172658a96ab141d8e1102c0f7855c587977c38
Evgeni <my@example.com>
2021-11-01 13:52:51 +0300

    first&footer(without link)

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,39 @@
 <template>
   <div id="app" class="back">
-    <div id="about">
-      <h1>Hi 👋</h1>
-      <p>My name is Sergei, I am an DevOps Engineer and you are on my webpage.</p>
+    <div id="about" class="align-self-start position-relative" :style="{'height': `${heightAbout}px`}">
+      <!-- <div id="vertical"> -->
+        <div class="position-absolute top-50 start-50 translate-middle name">
+          <p id="me">Serov Sergei</p>
+          <div class="desc">
+            <p>My name is Sergei, I am an DevOps Engineer and you are on my webpage.</p>
+          </div>
+        </div>
+      <!-- </div> -->
+    </div>
+    <div id="resume" class="align-self-start position-relative" :style="{'height': `${heightAbout}px`}">
+      <!-- <div id="vertical"> -->
+        <div class="position-absolute top-50 start-50 translate-middle">
+          <h3>Hi 👋</h3>
+          <p>My name is Sergei, I am an DevOps Engineer and you are on my webpage.</p>
+        </div>
+      <!-- </div> -->
     </div>
     <img alt="Vue logo" src="./assets/logo.png">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    
+    <!-- footer -->
+    <div class="footer">
+      <div class="d-flex align-items-center justify-content-between" style="height: 100%;">
+        <div class="p-1 text-muted vertical">
+          <p class="year">© 2021</p>
+        </div>
+        <div class="p-1 text-muted vertical">
+          <font-awesome-icon icon="fa-brands fa-github" class="icons"/>
+          <font-awesome-icon icon="fa-brands fa-linkedin" class="icons"/>
+          <font-awesome-icon icon="fa-brands fa-facebook" class="icons"/>
+          <font-awesome-icon icon="fa-brands fa-instagram" class="icons"/>
+        </div>
+      </div>
+    </div>
     <!-- borders -->
     <div class="line top"></div>
     <div class="line bottom"></div>
@@ -29,6 +56,14 @@ export default {
       name: 'Sergei',
     }
   },
+  mounted(){
+    // alert(document.documentElement.clientHeight);
+  },
+  computed: {
+    heightAbout(){
+      return document.documentElement.clientHeight
+    }
+  },
 }
 </script>
 
@@ -39,20 +74,68 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 #app #about{
-  color: blueviolet;
+    width: 90%;
+    margin: 0 auto;
+    height: 100%;
 }
+#app #about .name p#me{
+    color:#f0f0f0;
+    font-size: 4.7em;
+    font-weight: 700;
+    font-stretch: extra-expanded;
+}
+#app #about .name .desc p{
+    color: #a2a2a6;
+    font-size: 13px;
+}
+#app .vertical{
+  display: table-cell !important;
+  vertical-align: middle !important;
+}
+
+/* footer */
+
+#app .footer{
+  position: fixed;
+  bottom: 30px;
+  left: 30px;
+  height: 50px;
+  width: calc(100% - 60px);
+  padding: 0;
+  background-color: #31313a;
+}
+#app .footer p{
+  margin: 0;
+}
+#app .footer .year{
+  padding: 15px;
+}
+#app .footer .icons{
+  color: white;
+  font-size: 1.3em;
+  padding: 5px;
+  display: inline-block;
+  vertical-align: middle;
+}
+#app .footer .icons:hover{
+  color: #f26b38;
+  transition-duration: 0.5s;
+  transform: scale(1.3);
+  padding: 5px;
+}
+
+/* lines */
 #app .line{
   background: #26262d;
   content: '';
   position: fixed;
-  background: #26262d;
   z-index: 105;
 }
 #app .line.top{
-    /* left: 0; */
+    left: 0;
     top: 0;
     width: 100%;
     height: 30px;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebook,
+  faGithub,
+  faInstagram,
+  faLinkedin
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faBars, faFacebook, faGithub, faInstagram, faLinkedin);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+
+
 
 Vue.config.productionTip = false
 
