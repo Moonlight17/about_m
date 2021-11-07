561fc681b48ecae35a5223102f543e71e21b097c
Sergey Serov <serov@ancprotek.ru>
2021-11-07 12:50:28 +0300

    Created SelfEdu.vue

---
+++ src/components/selfedu.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <h1>SELF</h1>
+      <div class="com-sm-12 col-lg-12">
+        <div id="SelfEdu" class="SelfEdu">
+          
+        </div>
+      </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: 'SelfEdu',
+  props: {
+    me: Object
+  }
+}
+</script>
+
+<style scoped>
+#SelfEdu{
+
+}
+.SelfEdu{
+  
+}
+</style>
