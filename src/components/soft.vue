d1c63d5bb36d48d6c1e3c7312df7e62430ede491
Evgeni <my@example.com>
2021-11-08 16:59:12 +0300

    removed redundant class

--- src/components/soft.vue
+++ src/components/soft.vue
@@ -1,7 +1,7 @@
 <template>
-    <div id="softs" class="softs row justify-content-md-center">
+    <div id="softs" class="row justify-content-md-center">
       <p id="title">Soft Skills</p>
-      <div class="com-sm-12 col-lg-12">
+      <div class="col-sm-12 col-lg-12">
         <div id="SoftSkills" class="SoftSkills">
           <p id="desc">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem harum unde molestias consectetur eos cum quod doloremque molestiae, et aliquid quisquam magnam earum iste ab quis, odio fugit accusamus porro.
@@ -52,33 +52,19 @@ export default {
   color: #b4b4b4;
   position: relative;
   width: 100%;
-  padding: 0 8%;;
+  padding: 0 10%;
 }
 #title{
     text-align: left;
     font-size: 1.4em;
 }
 #desc{
-  padding: 15px 30px 0px 30px;
+  padding: 15px 0px 0px 0px;
   text-align: justify;
-}
-.mini_title{
-    width: 30%;
-    margin: 0 auto;
-    font-size: 1.2em;
-    padding: 0;
-    padding: 0 7%;
-    border: 2px solid #b4b4b4;
-}
-.softs{
-  color: #b4b4b4;
-  position: relative;
-  width: 100%;
-  padding: 0 80px 150px 80px;
 }
 .soft{
   text-align: left;
-  padding: 15px 30px 0px 30px;
+  padding: 15px 0px 0px 0px;
 }
 .soft p{
   padding-bottom: 0px;
